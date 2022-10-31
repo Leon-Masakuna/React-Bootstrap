4ee4d0f49641cc8dd12dcec327f169f35b940f36
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-10-31 17:56:18 +0100

    feat : learning react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
 import "./App.css";
+import { Button, Alert, Accordion, Badge } from "react-bootstrap";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey! I am learning React Bootstrap</h1>
+    <div className="mt-3">
+      <Navbar />
+      <h2>Hey! I am learning react-bootstrap</h2>
+      <Button variant="primary">Primary</Button>{" "}
+      <Button variant="secondary">Secondary</Button>{" "}
+      <Button variant="success">Success</Button>{" "}
+      <Button variant="warning">Warning</Button>{" "}
+      <Button variant="danger">Danger</Button>{" "}
+      <Button variant="info">Info</Button>{" "}
+      <Button variant="light">Light</Button>{" "}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+      <Alert variant="success" className="mt-3">
+        This is a <Alert.Link href="#">an example link</Alert.Link>. Give it a
+        click if you like.
+      </Alert>
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <h1>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h1>
+      <h2>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h2>
+      <h3>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h3>
+      <h4>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h4>
+      <h5>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h5>
+      <h6>
+        Example heading <Badge bg="secondary">New</Badge>
+      </h6>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+/*
+  React Bootstrap configuration
+*/
+
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+/*
+  Run "sudo sysctl -w fs.inotify.max_user_watches=524288" if limit number causes problem
+*/
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
